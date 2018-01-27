db36fe7232b769318e10bd95e845fa80dd7b7489
AinaSG <ainasoga@gmail.com>
2018-01-27 16:00:52 +0100

    Stuffs

--- main.gd
+++ main.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var Persona = preload("res://persona.tscn")
+const PERSONA_WIDTH = 500
 
 func _ready():
-	BuzzControllerManager.init()
+	var screenSize = get_viewport().size
+	
+	position = Vector2(0, screenSize.y)
+	
+	var availableSpace = screenSize.x / 12
+	print(availableSpace)
+	for i in range(12):
+		
+		var p = Persona.instance()
+		add_child(p)
+		
+		var s = availableSpace/PERSONA_WIDTH
+		print(s)
+		p.position.x = i * availableSpace + availableSpace/2
+		p.player_id = i
 
-func _process(delta):
-	BuzzControllerManager.update_inputs()
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
--- persona.gd
+++ persona.gd
@@ -14,19 +14,43 @@ onready var humer_e = get_node("B_Esquerra/Humer")
 onready var radi_d = get_node("B_Dret/Humer/Radi")
 onready var radi_e = get_node("B_Esquerra/Humer/Radi")
 
+onready var body = get_node("Cos")
+
 onready var pin_joint_humer_e = get_node("B_Esquerra/Humer/PinJoint2D")
 onready var pin_joint_radi_e = get_node("B_Esquerra/Humer/Radi/PinJoint2D")
 
 enum Direction { UP, DOWN }
+var swing_dir = 1
+var target_rotation_degrees = 0
 
 func _ready():
 	print("player %d ready" % player_id)
 	pin_joint_humer_e.softness = 1
 	pin_joint_radi_e.softness = 1
+	#body.angular_damp = INF
 	
 func _process(delta):
+	check_input()
+	
+	target_rotation_degrees += 5 * swing_dir * delta
+	if target_rotation_degrees > 25 or target_rotation_degrees < -25:
+		swing_dir *= -1
+		
+	#body.angular_velocity = 2 * body.rotation_degrees / (target_rotation_degrees) 
+	
+	
+func check_input():
+	if (player_id >= BuzzControllerManager.get_num_players()): return
 	var buttons = BuzzControllerManager.get_buttons_player(player_id)
 	
+	arm_movement(buttons)
+	head_shot(buttons)
+	
+func head_shot(buttons):
+	if buttons[0]:
+		print("PINYAU")
+	
+func arm_movement(buttons):
 	if buttons[1]: # blue   (P)
 		radi_active(Direction.UP)
 	elif buttons[2]: # orange (O)
@@ -40,7 +64,6 @@ func _process(delta):
 		humer_active(Direction.DOWN)
 	else:
 		humer_idle()
-		
 
 func radi_active(direction):
 	radi_d.angular_damp = 0.0
